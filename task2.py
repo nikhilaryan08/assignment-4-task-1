a87c09388e0acb8a9fc30a2425dbcca7f10d75cf
nikhilaryan08 <nikhilaryan24682468@gmail.com>
2025-07-24 08:33:40 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+# 1. Take user's first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# 2. Concatenate to create full name
+full_name = first_name + " " + last_name
+
+# 3. Print personalized greeting
+print("Hello,", full_name + "! Welcome to the Python world.")
